ad31b91b7694c88548ab23c3039db3689bcb5ee1
Advanced VLSI Lab, Silicon Institute of Technology <46992570+silicon-vlsi@users.noreply.github.com>
2024-03-29 17:16:51 +0530

    changed module name to spec.

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_silicon_tinytapeout_lm07 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
