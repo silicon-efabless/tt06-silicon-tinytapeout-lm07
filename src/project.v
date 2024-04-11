6e8914f7352fb0548b5fcc805a795412288cd059
Priyansu sahoo <85453216+Priyansu122@users.noreply.github.com>
2024-04-11 18:03:15 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -11,10 +11,14 @@
 `define CS_HIGH_COUNT   5'd20
 `define SPI_LATCH_COUNT 5'd22
 `define MAX_COUNT       5'd28
-`define SPI_IDLE	    2'b00
-`define SPI_READ	    2'b01
-`define SPI_LATCH	    2'b10
-
+//
+`define SPI_IDLE	2'b00
+`define SPI_READ	2'b01
+`define SPI_LATCH	2'b10
+//
+`define DISP_CORF	2'b00
+`define DISP_LSB	2'b01
+`define DISP_MSB	2'b10
 
 `define default_netname none
 
@@ -32,8 +36,10 @@ module tt_um_silicon_tinytapeout_lm07 (
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_oe  = 8'b00011111;
-  assign uio_out[7:5] = 3'b000;
+  // DOUBLE CHECK THIS. The enables may not be auto checked. 
+  assign uio_oe  = 8'b00111011;
+  assign uio_out[7:6] = 2'b00;
+  //assign uio_out[2] = 1'b0;//NO NEED
 //-------------------------------------------------------------------------------------
 
 //--------------Declaration of intenal signal to ports--------------
@@ -53,21 +59,20 @@ module tt_um_silicon_tinytapeout_lm07 (
   assign uo_out[6] = dataSeg[6];        //7-seg G
   assign uo_out[7] = dataSeg[7];        //7-seg DP
   
-  
+
     // SPI signals
   assign uio_out[0] = CS;              //CS-->chip select for LM70
   assign uio_out[1] = SCK;             //SCK--> clock for LM70
-  assign uio_out[2] = sel_ext[0];      //LSB
-  assign uio_out[3] = sel_ext[1];      //MSB
-  assign uio_out[4] = sel_ext[2];      //C/F
-  assign SIO = uio_in[5];              //SDI/MISO Data input
+  assign SIO = uio_in[2];              //SDI/MISO Data input
+  assign uio_out[3] = sel_ext[0];      //C/F
+  assign uio_out[4] = sel_ext[1];      //LSB
+  assign uio_out[5] = sel_ext[2];      //MSB
 
   wire SIO;
   wire CS;
   reg SCK;
   reg [7:0] dataSeg;
   wire sel_CorF;
-  
 
   reg [7:0] shift_reg;
   reg [7:0] tempC_bin_latch;
@@ -75,113 +80,77 @@ module tt_um_silicon_tinytapeout_lm07 (
   wire [7:0] tempCorF;
   reg [1:0] spi_state;
   reg [4:0] count;
+
   wire [3:0] bcd_msb;
   wire [3:0] bcd_lsb;
+  wire bcd_lsb_carry;
   wire [3:0] bcd_out;
+  wire [3:0] bcd_data;
+  wire [3:0] bcd_CorF;
+  wire data_state;
+  wire data_sel;
   
-  wire [2:0] sel_ext;       //sel_ext[1]-MSB/sel_ext[0]-LSB/sel_ext[2]=output port to slect 7 segemnt which will display temp unit
-  wire sel_ext_seg;              
-  // input signal for ui[0]-->ui[0]=1:use external 7 segment/=0:use demoboard 7 segment
-  wire sel_ob_LSB;               
-  // input signal for ui[1]-->ui[1]=1:show LSB/:show MSB in on board 7 segment hence ui[0] = 0 for this
+  wire [2:0] sel_ext;       //sel_ext[0]->CorF, [1]->LSB, [2]->MSB
 
-  //------------------------------------------------------------------------
-  /* NOTE :
-  Decoder logic to provide required signals for the the control MUX
-  */
-  reg [1:0] muxCtrl;
-  always @(*)
-    begin
-      casez({sel_ext_seg,sel_ob_LSB,sel_CorF,dispState})
-        5'b00??? : muxCtrl <= 2'b00;
-        5'b01??? : muxCtrl <= 2'b01;
-        
-        5'b1?001 : muxCtrl <= 2'b00;
-        5'b1?010 : muxCtrl <= 2'b01;
-        5'b1?011 : muxCtrl <= 2'b10;//send celcius bcd code to decoder
-        
-        5'b1?101 : muxCtrl <= 2'b00;
-        5'b1?110 : muxCtrl <= 2'b01;
-        5'b1?111 : muxCtrl <= 2'b11;//send farhenite bcd code to decoder
-        
-        default  : muxCtrl <= 2'b00;//default will only show msb    
-      endcase
-    end
-  //-----------------------------------------------------------------------------------
-  /* Note :
-  
-  MSB ----------|\
-                | \
-  LSB ----------|  \
-                |   |
-  Celcius       |   |---[Bcd to 7 segment decoder]--/-->output line
-  bcd code -----|   |                                8
-                |   |
-  Farhenite     |  /
-  bcd code -----| /|
-                |/ | 
-                   | [1:0]muxCtrl (from above mux)
-                 
-  ////////////////////////////////////////////////////////////////////////////////////////
-  */
-  reg [3:0] bcd_reg;
-  assign bcd_out = bcd_reg;
-  always @(*)
-    begin
-      case(muxCtrl)
-        2'b00 : bcd_reg <= bcd_msb;
-        2'b01 : bcd_reg <= bcd_lsb;
-        2'b10 : bcd_reg <= 4'b0000; //C
-        2'b11 : bcd_reg <= 4'b1000; //F
-      endcase
-    end
+  wire sel_ext_seg;         // assigned to ui[0]:'1':use external 7-seg '0':demoboard 7 segment
+  wire sel_ob_LSB;          // assigned to ui[1]:'1': display LSB on the demoboard 7-seg
+  reg lsb_sel;
+  wire lsb_state;
+
+  // Code below is immplementation of 2-stage MUXes to select LSB/MSB and C/F
+  assign data_state = (dispState == `DISP_LSB) || (dispState == `DISP_MSB);
+  assign data_sel = ~sel_ext_seg || data_state;
+  //
+  assign lsb_state = (dispState == `DISP_LSB);
+  //
+  always @(*) begin
+    case({sel_ext_seg, sel_ob_LSB, lsb_state}) 
+      3'b000: lsb_sel = 1'b0;
+      3'b001: lsb_sel = 1'b0;
+      3'b010: lsb_sel = 1'b1;
+      3'b011: lsb_sel = 1'b1;
+      
+      3'b100: lsb_sel = 1'b0;
+      3'b110: lsb_sel = 1'b0;
+      3'b101: lsb_sel = 1'b1;
+      3'b111: lsb_sel = 1'b1;
+      default: lsb_sel = 1'b0;
+    endcase
+  end
+      
+  assign bcd_data = lsb_sel ? bcd_lsb : bcd_msb + bcd_lsb_carry;
+  assign bcd_CorF = sel_CorF ? 4'b0001 : 4'b0000; //0-> C , 1-> F 
+  assign bcd_out = data_sel ? bcd_data : bcd_CorF;
+
   //--------------------------------------------------------------------------
- 
   // BCD ot 7-segment converter
-  /* NOTE :
-  a,b,c,d these 4 bits are sufficient to handle any overflow
-  as we have 2 or 3 degree error only the overflow can go upto
-  12 or 13 only also we have default as 9 so it cannot be also useful 
-  */
-  
+  // After deciamal '9', it wraps to '0' again with a carryover to MSB
   always @(*) begin//represents combinational logic
-    case (bcd_out)
-      4'b0000: begin 
-        dataSeg[7:3] <= 5'b00111;
-        case(dispState[0] && dispState[1])
-          1'b0: dataSeg[2:1] <= 2'b11;
-          1'b1: dataSeg[2:1] <= 2'b00;
-        endcase
-        dataSeg[0] <= 1'b1;
-      end
-      4'b0001: dataSeg = 8'b00000110;   //1 0x06  
-      4'b0010: dataSeg = 8'b01011011;	//2 0x5B	
-      4'b0011: dataSeg = 8'b01001111;	//3 0x4F
-      4'b0100: dataSeg = 8'b01100110;	//4 0x66
-      4'b0101: dataSeg = 8'b01101101;	//5 0x6D
-      4'b0110: dataSeg = 8'b01111101;	//6 0x7D
-      4'b0111: dataSeg = 8'b00000111;	//7 0x07
-	    4'b1000: dataSeg = {4'b0111,((dispState[0] && dispState[1]) ? 3'b000 : 3'b111),1'b1};	//8 0x7F
-      4'b1001: dataSeg = 8'b01101111;	//9 0x6E
-      4'b1010: dataSeg = 8'b01101111;	//9
-      4'b1011: dataSeg = 8'b01101111;	//9
-      4'b1100: dataSeg = 8'b01101111;	//9
-      4'b1101: dataSeg = 8'b01101111;	//9
-      4'b1110: dataSeg = 8'b01101111;	//9
-      4'b1111: dataSeg = 8'b01101111;	//9
-      default: dataSeg = 8'b01101111; // Default case to avoid latches
+    case ({data_sel,bcd_out})
+	//Temperature Display
+        5'b10000: dataSeg = 8'b00111111; //0 0x3F
+        5'b10001: dataSeg = 8'b00000110; //1 0x06
+        5'b10010: dataSeg = 8'b01011011; //2 0x5B	
+        5'b10011: dataSeg = 8'b01001111; //3 0x4F
+        5'b10100: dataSeg = 8'b01100110; //4 0x66
+        5'b10101: dataSeg = 8'b01101101; //5 0x6D
+        5'b10110: dataSeg = 8'b01111101; //6 0x7D
+        5'b10111: dataSeg = 8'b00000111; //7 0x07
+        5'b11000: dataSeg = 8'b01111111; //8 0x7F
+        5'b11001: dataSeg = 8'b01101111; //9 0x6F
+        5'b11010: dataSeg = 8'b00111111; //0
+        5'b11011: dataSeg = 8'b00000110; //1
+        5'b11100: dataSeg = 8'b01011011; //2
+        5'b11101: dataSeg = 8'b01001111; //3
+        5'b11110: dataSeg = 8'b01100110; //4 
+        5'b11111: dataSeg = 8'b01101101; //5
+	//Centigrade or Farahneit 
+        5'b00000: dataSeg = 8'b00111001;	//C 0x39 
+        5'b00001: dataSeg = 8'b01110001;	//F 0x71 
+	//
+        default:  dataSeg = 8'b00000110; // 0x06 Default case to avoid latches
     endcase
 end
-  
-  //------------------DATA SELECTION AND CONVERSION SECTION---------------------------
-  /* NOTE :
-  -> This segment takes the raw data 
-  -> then mux will select if the data to be displayed in farhenite or celcius
-  -> if farhenite the conversion formula will convert that then it will be assigned to "tempCorf" wire.
-  -> else the raw data will be assigned to "tempCorf" wire
-  -> Then it will pass through raw to bcd data conversion algrithm and overall data will be divided to
-  bcd_lsb and bcd_msb
-  */
   
   // Course C-to-F conversion
   // temp(F) = 2*C + 32  (Accurate: 9*C/5 +32)
@@ -195,7 +164,8 @@ end
   assign bcd_msb = (tempCorF + (tempCorF>>1))>>4;
   //LSB = temp - 10*MSB = temp - (8*MSB + 2*MSB)
   assign bcd_lsb = tempCorF - ((bcd_msb<<3) + (bcd_msb<<1)); 
-  //-------------------------------------------------------------------------------------
+  // Capturing overflow bit
+  assign bcd_lsb_carry = bcd_lsb > 4'h9;
  
   //shift register for the input (SIO)
   always @(posedge SCK or negedge rst_n)
@@ -223,20 +193,21 @@ assign CS = ~(spi_state == `SPI_READ);
 // Signals to turn ON the external 7 segment display 
 // Assigned to bidirectional output ports
 
-  assign sel_ext[1] =  ~dispState[1] & dispState[0] & sel_ext_seg; //01--> MSB
-  assign sel_ext[0] =   dispState[1] & ~dispState[0]& sel_ext_seg; //10--> LSB
-  assign sel_ext[2] =   dispState[1] & dispState[0] & sel_ext_seg; //11--> C/F
+  assign sel_ext[0] = (dispState == `DISP_CORF) & sel_ext_seg; //00->CORF
+  assign sel_ext[1] = (dispState == `DISP_LSB)  & sel_ext_seg; //01->LSB
+  assign sel_ext[2] = (dispState == `DISP_MSB)  & sel_ext_seg; //10->MSB
   
 // 1 for MSB 0 for LSB always --> our covention
 
 // 3-spi_state (IDLE, READ, LATCH) spi_state-machine
   reg [1:0] dispState;
+
 always @(posedge clk or negedge rst_n)
   if (~rst_n)
       begin	    
         spi_state <= `SPI_IDLE;
         tempC_bin_latch <= 8'b0;
-        dispState <= 0;
+        dispState <= `DISP_CORF;
       end
   else if ((count >= `CS_LOW_COUNT) && (count < `CS_HIGH_COUNT) )
       begin
@@ -248,10 +219,10 @@ always @(posedge clk or negedge rst_n)
         tempC_bin_latch <= shift_reg<<1;
         //small state machine to external 3 7 segment display
         case(dispState)
-          2'b00: dispState <= 2'b01; //00 -> reset state
-          2'b01: dispState <= 2'b10; //01 -> Show MSB state
-          2'b10: dispState <= 2'b11; //10 -> show LSB state
-          2'b11: dispState <= 2'b01; //11 -> show temp unit
+          `DISP_CORF: dispState <= `DISP_LSB; 
+          `DISP_LSB: dispState <= `DISP_MSB; 
+          `DISP_MSB: dispState <= `DISP_CORF; 
+          default: dispState <= `DISP_CORF; 
         endcase  
       end
   else 
